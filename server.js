f83c25e3b8c650e3704a8a0438258b16188da794
Jun Kheng <junkheng@gmail.com>
2020-02-13 12:44:14 +0800

    create user model, db, routes with bcrypt and jwt

--- server.js
+++ server.js
@@ -53,6 +53,7 @@ class HandlerGenerator {
     const express = require('express')
     const app = express()
     const todoRouter = require('./routes/todo/todoRoutes')
+    const userRouter = require('./routes/user/userRoutes')
 
     // To parse json
     const bodyParser = require('body-parser')
@@ -63,6 +64,7 @@ class HandlerGenerator {
     app.use(bodyParser.urlencoded({ extended: true }))
     app.use(bodyParser.json())
     app.use('/todo', todoRouter)
+    app.use('/user', userRouter)
 
     app.post('/login', handlers.login)
     app.get('/', middleware.checkToken, handlers.index)
